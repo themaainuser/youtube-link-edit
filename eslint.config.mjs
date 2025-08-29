6b465764d0144d63c26f466c69008b3319ee2a46
themaainuser <kartikshingala05@gmail.com>
2025-08-29 12:01:59 +0530

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
+// C:\Users\Kartik\OneDrive\Desktop\YoutubeLinkwithSeconds\eslint.config.mjs
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import { globalIgnores } from "eslint/config";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,6 +13,21 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  globalIgnores(
+    [
+      // Ignore specific files causing warnings
+      "app/components/githubButton.tsx",
+      "app/components/ui/liquid-gradient.tsx",
+      "app/page.tsx",
+
+      // Optional: ignore entire folders (example)
+      // "app/components/**",
+
+      // You can also ignore all files of a type if needed:
+      // "**/*.ts",
+    ],
+    "Ignore ESLint Warnings During Build",
+  ),
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
