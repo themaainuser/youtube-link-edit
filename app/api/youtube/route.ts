919d660ac224a93d7c03d5d199d1e20207db1bc6
themaainuser <kartikshingala05@gmail.com>
2025-08-29 11:45:13 +0530

    eslint

--- app/api/youtube/route.ts
+++ app/api/youtube/route.ts
@@ -48,7 +48,7 @@ export async function POST(req: NextRequest) {
     }
   } catch (error) {
     return NextResponse.json(
-      { error: "Internal Server Error" },
+      { error: "Internal Server Error" + error },
       { status: 500 },
     );
   }
